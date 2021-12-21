15dca3006ec4a3b7e45cd0248c711d18dce0a261
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-12-21 23:17:33 +0900

    Initial Commit

---
+++ RealmTodo/RealmTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTodoApp.swift
+//  RealmTodo
+//
+//  Created by Masato Ishikawa on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RealmTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
